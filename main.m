71da442430597d111d48b2455172f757fca19ff3
svaria <shaanvaria@MacBook-Air.local>
2015-03-03 17:31:35 -0500

    using angles at lowpoint

--- main.m
+++ main.m
@@ -5,12 +5,12 @@ global width height;
 width = 640;
 height = 480;
 
-rgb_img = imread('../a/rgba4.png');
+rgb_img = imread('a/rgba2.png');
 
 
-depth_array = read_vdi('../a/vdia4');
+depth_array = read_vdi('a/vdia2');
 depth_image = depth1(depth_array);
-filtered_depth_image_high = threshold(depth_image, 0.8);
+%filtered_depth_image_high = threshold(depth_image, 0.8);
 depth_thresh = 0.8;
 filtered_depth_image_low = threshold(depth_image, depth_thresh);
 
@@ -20,23 +20,25 @@ filtered_depth_image_low = threshold(depth_image, depth_thresh);
 [cx_h, cy_h, num_points] = avg_centroid(filtered_depth_image_high);
 
 %Eliminate points too far away
-r = 180;
-depth_far_from_c_masked = ...
-    mask_points_outside_r(depth_image, cx_h, cy_h, r);
-thresholded_on_color = threshold(rgb_img, 0.5);
-color_far_from_c_masked = ...
-    mask_points_outside_r(thresholded_on_color, cx_h, cy_h, r);
+%r = 180;
+%depth_far_from_c_masked = ...
+%    mask_points_outside_r(depth_image, cx_h, cy_h, r);
+%thresholded_on_color = threshold(rgb_img, 0.5);
+%color_far_from_c_masked = ...
+%    mask_points_outside_r(thresholded_on_color, cx_h, cy_h, r);
 
 
-imshow(color_far_from_c_masked);
+%imshow(color_far_from_c_masked);
 
-thresholded_on_depth_and_color = combine_color_depth(color_far_from_c_masked,...
-    depth_far_from_c_masked, depth_thresh);
+%thresholded_on_depth_and_color = combine_color_depth(color_far_from_c_masked,...
+%    depth_far_from_c_masked, depth_thresh);
 
 %imshow(thresholded_on_depth_and_color);
 
 [left_most, right_most, highest, lowest] = ...
-    get_corners(filtered_depth_image_low, [cx, cy]);
+    get_corners(filtered_depth_image_low, [cx_h, cy_h])
 
-
-
+vert = lowest(2)-left_most(2)
+hor = lowest(1)-left_most(1)
+deg = atan(vert/hor)
+imshow(filtered_depth_image_low)
